8f71e03103344353cb8fb3a800960303baa7e19a
Andrey Fadeev <andfadeev@gmail.com>
2024-11-07 21:43:24 +0000

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,52 +1,57 @@
-import {Hono} from 'hono'
-import {createBunWebSocket} from 'hono/bun'
-import type {ServerWebSocket} from 'bun'
+import { Hono } from "hono";
+import { createBunWebSocket } from "hono/bun";
+import type { ServerWebSocket } from "bun";
 
-const app = new Hono()
+const app = new Hono();
 
-const {upgradeWebSocket, websocket} = createBunWebSocket<ServerWebSocket>()
+const { upgradeWebSocket, websocket } = createBunWebSocket<ServerWebSocket>();
 
-app.get('/', (c) => {
+app
+  .get("/", (c) => {
     return c.html(
-        <html>
+      <html>
         <head>
-            <meta charset='UTF-8'/>
-            <title>Hono HTMX Websockets</title>
-            <script src="https://unpkg.com/htmx.org@1.9.12"
-                    integrity="sha384-ujb1lZYygJmzgSwoxRggbCHcjc0rB2XoQrxeTUQyRjrOnlCoYta87iKBWq3EsdM2"
-                    crossOrigin="anonymous"/>
-            <script src="https://unpkg.com/htmx.org@1.9.12/dist/ext/ws.js"/>
+          <meta charset="UTF-8" />
+          <title>Hono HTMX Websockets</title>
+          <script
+            src="https://unpkg.com/htmx.org@1.9.12"
+            integrity="sha384-ujb1lZYygJmzgSwoxRggbCHcjc0rB2XoQrxeTUQyRjrOnlCoYta87iKBWq3EsdM2"
+            crossOrigin="anonymous"
+          />
+          <script src="https://unpkg.com/htmx.org@1.9.12/dist/ext/ws.js" />
         </head>
-        <body>
-        <div>
-            <h1>Order Status:</h1>
-            <div hx-ext="ws" ws-connect="/order/1/status">
-                <div id="orderUpdate" hx-swap-oob="beforeend"></div>
-            </div>
-        </div>
+        <body style="background:black">
+          <div>
+            <h1 style="color:white">Order Status:</h1>
+            {/* <div hx-ext="ws" ws-connect="/order/1/status">
+              <div id="orderUpdate" hx-swap-oob="beforeend"></div>
+            </div> */}
+          </div>
         </body>
-        </html>
-    )
-})
-    .get(
-        '/order/1/status',
-        upgradeWebSocket((c) => {
-            let intervalId: ReturnType<typeof setInterval>;
-            return {
-                onOpen(_event, ws) {
-                    intervalId = setInterval(() => {
-                        console.log("Sending order update")
-                        ws.send(`<div id="orderUpdate" hx-swap-oob="beforeend"><div>Order Updated</div></div>`)
-                    }, 2000)
-                },
-                onClose() {
-                    clearInterval(intervalId)
-                },
-            }
-        })
-    )
+      </html>,
+    );
+  })
+  .get(
+    "/order/1/status",
+    upgradeWebSocket((c) => {
+      let intervalId: ReturnType<typeof setInterval>;
+      return {
+        onOpen(_event, ws) {
+          intervalId = setInterval(() => {
+            console.log("Sending order update");
+            ws.send(
+              `<div id="orderUpdate" hx-swap-oob="beforeend"><h2 style="color:white">Order Updated</h2></div>`,
+            );
+          }, 2000);
+        },
+        onClose() {
+          clearInterval(intervalId);
+        },
+      };
+    }),
+  );
 
 export default {
-    fetch: app.fetch,
-    websocket,
-}
+  fetch: app.fetch,
+  websocket,
+};
